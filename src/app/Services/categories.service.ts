161577ba1d8e15c69984d34e8a00f7a637de2ccc
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-09-30 02:21:24 +0200

    Update

--- src/app/Services/categories.service.ts
+++ src/app/Services/categories.service.ts
@@ -1,9 +1,25 @@
+import { environment } from 'src/environments/environment.development';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { iCategory } from '../Models/iCategory';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoriesService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getAllCategories(): Observable<iCategory[]> {
+    return this.httpClient
+      .get<iCategory>(`${environment.BaseApiURL}/categories`)
+      .pipe(map((res: any) => res.data.documents));
+  }
+
+  deleteCategory(categoryId: number): Observable<void> {
+    console.log('service', categoryId);
+    return this.httpClient.delete<void>(
+      `${environment.BaseApiURL}/categories/${categoryId}`
+    );
+  }
 }
--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { iCategory } from 'src/app/Models/iCategory';
+import { CategoriesService } from 'src/app/Services/categories.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  styleUrls: ['./categories.component.css'],
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categories: iCategory[] = [];
+  constructor(
+    private categoriesServ: CategoriesService,
+    private spinner: NgxSpinnerService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    this.spinner.show();
 
+    this.categoriesServ.getAllCategories().subscribe((data) => {
+      //console.log(data);
+      this.categories = data;
+      this.spinner.hide();
+    });
+  }
+
+  getImage(photo: string): string {
+    return `http://127.0.0.1:4000/img/categories/${photo}`;
+  }
+
+  deleteCategory(id: number) {
+    const confirmDelete = confirm(
+      'Are you sure you want to delete this category?'
+    );
+    if (confirmDelete) {
+      this.categoriesServ.deleteCategory(id).subscribe({
+        next: () => {
+          confirm('Successfully Deleted');
+          this.categoriesServ.getAllCategories().subscribe((data) => {
+            //console.log(data);
+            this.categories = data;
+          });
+        },
+      });
+      //console.log('component', id);
+      this.router.navigate(['/categories']);
+    }
+  }
 }
