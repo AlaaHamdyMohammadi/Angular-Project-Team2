efbe9feff6b5a51359746acab304e04d8f06b2c3
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-09-30 02:02:27 +0200

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UsersComponent } from './components/users/users.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { SubCategoriesComponent } from './components/sub-categories/sub-categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UsersComponent,
+    LoginPageComponent,
+    CoursesComponent,
+    CategoriesComponent,
+    SubCategoriesComponent
   ],
   imports: [
     BrowserModule,
