e6265ad5275989c8657e300f1277671501550e21
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-09-30 02:25:41 +0200

    Update

--- src/app/Services/sub-categories.service.ts
+++ src/app/Services/sub-categories.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { Observable, map } from 'rxjs';
+import { iSubCategory } from '../Models/iSubCategory';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SubCategoriesService {
-
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+  getAllSubCategories(): Observable<iSubCategory[]> {
+    return this.httpClient
+      .get<iSubCategory>(`${environment.BaseApiURL}/subCategories`)
+      .pipe(map((res: any) => res.data.subCategories));
+  }
+  deleteSubCategory(subCategoryId: number): Observable<void> {
+    console.log('service', subCategoryId);
+    return this.httpClient.delete<void>(
+      `${environment.BaseApiURL}/subCategories/${subCategoryId}`
+    );
+  }
 }
--- src/app/components/sub-categories/sub-categories.component.ts
+++ src/app/components/sub-categories/sub-categories.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { iSubCategory } from 'src/app/Models/iSubCategory';
+import { SubCategoriesService } from 'src/app/Services/sub-categories.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sub-categories',
   templateUrl: './sub-categories.component.html',
-  styleUrls: ['./sub-categories.component.css']
+  styleUrls: ['./sub-categories.component.css'],
 })
 export class SubCategoriesComponent {
+  subCategories: iSubCategory[] = [];
+  constructor(
+    private subCategoriesServ: SubCategoriesService,
+    private spinner: NgxSpinnerService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    this.spinner.show();
 
+    this.subCategoriesServ.getAllSubCategories().subscribe((data) => {
+      //console.log(data);
+      this.subCategories = data;
+      this.spinner.hide();
+    });
+  }
+
+  deleteSubCategory(id: number) {
+    const confirmDelete = confirm(
+      'Are you sure you want to delete this category?'
+    );
+    if (confirmDelete) {
+      this.subCategoriesServ.deleteSubCategory(id).subscribe({
+        next: () => {
+          confirm('Successfully Deleted');
+          this.subCategoriesServ.getAllSubCategories().subscribe((data) => {
+            //console.log(data);
+            this.subCategories = data;
+          });
+        },
+      });
+      this.router.navigate(['/subCategories']);
+    }
+  }
 }
