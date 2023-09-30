80a49f212df44c0a2bf232775a74f45aae118865
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-09-30 02:15:12 +0200

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { UsersComponent } from './components/users/users.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { SubCategoriesComponent } from './components/sub-categories/sub-categories.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent, title: 'Login Page' },
+  { path: 'courses', component: CoursesComponent, title: 'Courses Page' },
+  { path: 'users', component: UsersComponent, title: 'Users Page' },
+  {
+    path: 'categories',
+    component: CategoriesComponent,
+    title: 'Categories Page',
+  },
+  {
+    path: 'subCategories',
+    component: SubCategoriesComponent,
+    title: 'SubCategories Page',
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
