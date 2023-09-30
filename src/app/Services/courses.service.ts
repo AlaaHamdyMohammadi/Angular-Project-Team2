78907840efafdaa9683adbaa5e878672c419543b
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-09-30 02:23:47 +0200

    Update

--- src/app/Services/courses.service.ts
+++ src/app/Services/courses.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { ICourse } from '../Models/iCourse';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getAllCourses(): Observable<ICourse[]> {
+    return this.httpClient
+      .get<ICourse[]>(`${environment.BaseApiURL}/courses`)
+      .pipe(map((res: any) => res.data.courses));
+  }
+  getCourseById(courseId: number): Observable<ICourse> {
+    return this.httpClient.get<ICourse>(
+      `${environment.BaseApiURL}/courses/${courseId}`
+    );
+  }
 }
--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICourse } from 'src/app/Models/iCourse';
+import { CoursesService } from 'src/app/Services/courses.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  courses: ICourse[] = [];
+  constructor(
+    private coursesServ: CoursesService,
+    private spinner: NgxSpinnerService
+  ) {}
+  ngOnInit(): void {
+    this.spinner.show();
+    this.coursesServ.getAllCourses().subscribe((data) => {
+      //console.log(data);
+      this.courses = data;
+      this.spinner.hide();
+    });
+  }
 
+  getImage(photo: string): string {
+    return `http://127.0.0.1:4000/img/courses/${photo}`;
+  }
 }
