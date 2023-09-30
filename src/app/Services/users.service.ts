b8a1ae1e119b340ef1ac64d9c69da1246e7af2b2
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-09-30 02:29:26 +0200

    Update

--- src/app/Services/users.service.ts
+++ src/app/Services/users.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { iUser } from '../Models/iUser';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  http = {};
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.http = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    };
+  }
+
+  getAllUsers(): Observable<iUser[]> {
+    return this.httpClient
+      .get<iUser>(`${environment.BaseApiURL}/users`)
+      .pipe(map((res: any) => res.data.documents));
+  }
+
+  deleteUser(userId: number): Observable<void> {
+    console.log('service', userId);
+    return this.httpClient.delete<void>(
+      `${environment.BaseApiURL}/users/${userId}`
+    );
+  }
 }
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { iUser } from 'src/app/Models/iUser';
+import { UsersService } from 'src/app/Services/users.service';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  users: iUser[] = [];
+  user: iUser = {} as iUser;
 
+  constructor(
+    private usersServ: UsersService,
+    private spinner: NgxSpinnerService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    this.spinner.show();
+
+    this.usersServ.getAllUsers().subscribe((data) => {
+      console.log(data);
+      this.users = data;
+      this.spinner.hide();
+    });
+  }
+
+  getImage(photo: string): string {
+    return `http://127.0.0.1:4000/img/users/${photo}`;
+  }
+
+  deleteUser(id: number) {
+    const confirmDelete = confirm('Are you sure you want to delete this user?');
+    if (confirmDelete) {
+      this.usersServ.deleteUser(id).subscribe({
+        next: () => {
+          confirm('Successfully Deleted');
+          this.usersServ.getAllUsers().subscribe((data) => {
+            this.users = data;
+            //console.log('Done');
+          });
+          this.router.navigate(['/users']);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+      //console.log("component", id);
+    }
+  }
 }
+
+/*
+
+{
+        next: () => {
+          confirm('Successfully Deleted');
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      }
+*/
