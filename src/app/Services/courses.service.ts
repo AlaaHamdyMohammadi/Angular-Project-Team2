eb4e7a664eed9dbfeb94711ac42d537d16a7d917
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-04 23:34:55 +0200

    Update

--- src/app/Services/courses.service.ts
+++ src/app/Services/courses.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { ICourse } from '../Models/iCourse';
@@ -8,23 +8,48 @@ import { environment } from 'src/environments/environment.development';
   providedIn: 'root',
 })
 export class CoursesService {
-  constructor(private httpClient: HttpClient) {}
+  http = {};
+  constructor(private httpClient: HttpClient) {
+    this.http = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    };
+  }
 
   getAllCourses(page: number = 1, limit: number = 15): Observable<ICourse[]> {
-    const params = new HttpParams().set('page', page.toString()).set('limit', limit.toString())
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('limit', limit.toString());
     return this.httpClient
-      .get<ICourse[]>(`${environment.BaseApiURL}/courses`, {params})
+      .get<ICourse[]>(`${environment.BaseApiURL}/courses`, { params })
       .pipe(map((res: any) => res.data.courses));
   }
+
   getCourseById(courseId: number): Observable<ICourse> {
     return this.httpClient.get<ICourse>(
       `${environment.BaseApiURL}/courses/${courseId}`
     );
   }
-  deleteCourse(courseId: number): Observable<void> {
-    console.log('service', courseId);
+
+  creatCourse(newCourse: FormData): Observable<any> {
+    return this.httpClient.post<any>(
+      `${environment.BaseApiURL}/courses`,
+      newCourse
+    );
+  }
+
+  //
+  updateCourse(updateCourse: FormData, id: number) {
+    console.log('in service', id);
+    return this.httpClient.patch<any>(
+      `${environment.BaseApiURL}/courses/${id}`,
+      updateCourse
+    );
+  }
+  //
+  deleteCourse(removeCourseId: number): Observable<void> {
+    console.log(removeCourseId);
     return this.httpClient.delete<void>(
-      `${environment.BaseApiURL}/courses/${courseId}`
+      `${environment.BaseApiURL}/courses/${removeCourseId}`
     );
   }
 }
