2bb7f94066c2277aa8f0f4281b8d5d27eab5fb3c
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-06 03:19:17 +0200

    Update

---
+++ src/app/Models/iInstructor.ts
@@ -0,0 +1,15 @@
+export interface iInstructor {
+  _id: number;
+  username: string;
+  email: string;
+  photo: string;
+  password: string;
+  passwordConfirm: string;
+  role: UserRole;
+}
+
+export enum UserRole {
+  User = 'user',
+  Instructor = 'instructor',
+  Admin = 'admin',
+}
